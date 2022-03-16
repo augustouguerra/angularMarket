6da019ac00e98bb6847ac50f2695c221c755ee0f
Augusto Guerra <augustouguerra@gmail.com>
2022-03-16 08:30:10 -0300

    navcomponent searchcomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,39 @@
+
+
+// MODULES
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// COMPONENTS
+import { SearchBarComponent } from './search-bar/search-bar.component';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { NavComponent } from './nav/nav.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// MATERIAL
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
+import { FormsModule } from '@angular/forms'; 
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-    NavComponent
+    NavComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
